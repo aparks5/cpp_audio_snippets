75b2551965db207f9c6ce729484fbd4c612c9b32
Anthony Parks <acousticscommander@gmail.com>
2021-08-27 10:31:39 -0400

    Update auto.cpp

--- modern_cpp/auto.cpp
+++ modern_cpp/auto.cpp
@@ -11,13 +11,5 @@ int main()
     auto max = 16;
     cout << val << "\n";
     cout << max << "\n";
-
-    // lambdas captured by reference
-    vector<int> vec{1,2,3,4,5};
-    auto total = 0;
-    auto accum = [&total](vector<auto> v) { for (auto i : v) {total += i;} return total;};
-    auto res = accum(vec);
-    cout << res;
-    return 0;
 }
 
